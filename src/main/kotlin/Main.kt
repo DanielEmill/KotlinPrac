59c198c235ded9f74ccd53c91eb19be9e2591b3e
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-02 15:03:37 -0400

    Variables Test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println("Hello World!")
+    var userName: String = "Daniel"
+    userName = "Emill"
+    var edad: Int = 21
+    var numfav = 1.14 // Aqui se utilizo variable sin declarar.
+
+    println("Hola $userName!, tu edad es $edad, tu numero favorito es $numfav")
 }
