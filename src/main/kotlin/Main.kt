7665606b88679bcc44c279151abcc76f1412190b
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-02 15:07:53 -0400

    Variables Test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,13 @@
+import kotlin.math.max
+
 fun main() {
     var userName: String = "Daniel"
     userName = "Emill"
     var edad: Int = 21
     var numfav = 1.14 // Aqui se utilizo variable sin declarar.
+    var numMax: Int = Int.MAX_VALUE
+    var numMin: Int = Int.MIN_VALUE
 
+    println("Numero maximo: $numMax \nNumero Minimo es: $numMin")
     println("Hola $userName!, tu edad es $edad, tu numero favorito es $numfav")
 }
